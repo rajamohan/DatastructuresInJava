0536a0cac29a46ba474000f3e7fa79ec022f1387
Rajamohan Sugumaran <sugum@GHLAPHR005-L.nlcads.nlcindia.com>
2026-09-11 01:31:47 +0530

    Adding delete logic to single linked list

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -49,6 +49,32 @@ class LinkedList{
         node.next = temp.next; // set newly created node as prev node to position +1 node
         temp.next = node; // set newly created node as next node to position -1 node
     }
+
+    public void deleteAt(int position){
+        if(position == 0){
+          System.out.println("Delete at begining");
+          Node temp = head; // Getting head
+          head = head.next; // Setting head as next element of head so it will delete 1st entry
+          temp.next = null;
+        }
+        else if(position == -1){
+            System.out.println("Delete at end");
+            Node temp = head;
+            while (temp.next!=tail){ // Iterate until pointer reaches previous node of tail node
+                temp = temp.next; // get the last before node
+            }
+            temp.next  = null;  // Remove next ref in 2nd last node
+            tail = temp; // set 2nd last node as tail
+        } else{
+            System.out.println("Deleted at position : " + position);
+            Node temp = head;
+            for(int i=0; i<position-1; i++){
+                temp = temp.next; // Reaches the previous node of position node
+            }
+            temp.next = temp.next.next; // Getting the next ref of position node and setting to position -1 nodes ref
+
+        }
+    }
 }
 
 public class SingleLinkedList {
@@ -57,6 +83,10 @@ public class SingleLinkedList {
         linkedList.insert(10);
         linkedList.insert(20);
         linkedList.insert(30);
+        linkedList.insert(40);
+        linkedList.insert(50);
+        linkedList.insert(60);
+
         linkedList.display();
         IO.println("-------------------------------------");
         linkedList.inserAtBegining(7);
@@ -66,6 +96,15 @@ public class SingleLinkedList {
         linkedList.display();
         IO.println("-------------------------------------");
         linkedList.insertAt(3, 15);
+        linkedList.display();
+        IO.println("-------------------------------------");
+        linkedList.deleteAt(0);
+        linkedList.display();
+        IO.println("-------------------------------------");
+        linkedList.deleteAt(-1);
+        linkedList.display();
+        IO.println("-------------------------------------");
+        linkedList.deleteAt(3);
         linkedList.display();
     }
 }
